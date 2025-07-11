3d17b88f11f88c11ada643e73ed10c253386c4bf
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-07-11 07:47:04 -0500

    typo

--- src/pipeline-GF-WM.sh
+++ src/pipeline-GF-WM.sh
@@ -59,6 +59,6 @@ feat design.fsf
 
 # Copy html to a different location to be a separate output
 mkdir "${out_dir}"/HTML
-cp -R "${feat_dir}"/GF-WM/*.html "${out_dir}"/HTML
-cp -R "${feat_dir}"/GF-WM/*.png "${out_dir}"/HTML
-cp -R "${feat_dir}"/GF-WM/.files "${out_dir}"/HTML
+cp -R "${feat_dir}"/GF-WM.feat/*.html "${out_dir}"/HTML
+cp -R "${feat_dir}"/GF-WM.feat/*.png "${out_dir}"/HTML
+cp -R "${feat_dir}"/GF-WM.feat/.files "${out_dir}"/HTML
